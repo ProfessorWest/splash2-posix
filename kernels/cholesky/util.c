b6105ae5dd6340192f9275aaf987c11328dd7907
Dr. West <pwest@cs.univ.edu>
2020-02-17 10:02:40 -0500

    first try at a static cholesky.

--- kernels/cholesky/util.c
+++ kernels/cholesky/util.c
@@ -26,6 +26,7 @@
 #include <unistd.h>
 #line 17
 #include <stdlib.h>
+#include <ctype.h>
 #line 17
 extern pthread_t PThreadTable[];
 #line 17
@@ -39,6 +40,39 @@ extern pthread_t PThreadTable[];
 #define AddMember(set, new) { long s, n; s = set; n = new; link[n] = link[s]; link[s] = n; }
 
 long maxm;
+
+/*
+typedef struct {
+	long n, m, *col, *startrow, *row;
+	double *nz;
+	} SMatrix;
+    */
+void printMatrix(SMatrix M) {
+	long hi, lo;
+	long i, j, k, tmp;
+
+    //for (int k = 0; k < M.n; k++) {
+    for (int k = 0; k < 2; k++) {
+	    hi = M.col[k+1];
+	    lo = M.col[k];
+        printf("lo = %d, hi = %d\n", lo, hi);
+	    for (i=lo; i<hi; i++) {
+            printf("%d ", M.row[i]);
+        }
+        printf("\n");
+    }
+    /*
+	for (i=lo; i<hi; i++) {
+		tmp = M.row[i];
+		j = i;
+		while (M.row[j-1] > tmp && j > lo) {
+			M.row[j] = M.row[j-1];
+			j--;
+			}
+		M.row[j] = tmp;
+		}
+        */
+}
 
 SMatrix NewMatrix(long n, long m, long nz)
 {
@@ -97,6 +131,8 @@ double Value(long i, long j)
   }
 }
 
+SMatrix ReadSparseStr(char *text, char *probName);
+extern const char *lshp;
 
 SMatrix ReadSparse(char *name, char *probName)
 {
@@ -115,6 +151,7 @@ SMatrix ReadSparse(char *name, char *probName)
 	}
 
 	if (!fp) {
+        return ReadSparseStr(lshp, probName);
 		Error("Error opening file\n");
 	}
 
@@ -198,6 +235,109 @@ SMatrix ReadSparse(char *name, char *probName)
 	return(F);
 }
 
+SMatrix ReadSparseStr(char *text, char *probName)
+{
+	long n, m, i, j;
+	long n_rows, tmp;
+	long numer_lines;
+	long colnum, colsize, rownum, rowsize;
+	char buf[100], type[4];
+	SMatrix M, F;
+
+	if (!text) {
+		Error("Can't read text.\n");
+	}
+
+    while (*text != '\n') { text++; }
+
+    strncpy(probName, text, 8);
+	probName[8] = 0;
+
+    while (*text != '\n') { text++; }
+
+	for (i=0; i<5; i++) {
+	  sscanf(text, "%ld", &tmp);
+      while (!isdigit(*text)) { text++; }
+	  if (i == 3)
+	    numer_lines = tmp;
+	}
+    while (*text != '\n') { text++; }
+    text++;
+
+	sscanf(text, "%3c", type);
+	type[3] = 0;
+
+	if (!(type[0] != 'C' && type[1] == 'S' && type[2] == 'A')) {
+	  fprintf(stderr, "Wrong type: %s\n", type);
+	  exit(0);
+	}
+
+    while (!isdigit(*text)) { text++; }
+	sscanf(text, "%ld", &n_rows);
+    text += 14;
+
+    while (!isdigit(*text)) { text++; }
+	sscanf(text, "%ld", &n);
+    text += 14;
+
+    while (!isdigit(*text)) { text++; }
+    text--;
+	sscanf(text, "%ld", &m);
+    text += 14;
+
+	if (tmp != 0)
+	  printf("This is not an assembled matrix!\n");
+	if (n_rows != n)
+	  printf("Matrix is not symmetric\n");
+
+
+	sscanf(text, "%16c", buf);
+    text += 16;
+	ParseIntFormat(buf, &colnum, &colsize);
+    while (*text != '(') { text++; }
+	sscanf(text, "%16c", buf);
+    text += 16;
+	ParseIntFormat(buf, &rownum, &rowsize);
+	sscanf(text, "%20c", buf);
+    text += 20;
+	sscanf(text, "%20c", buf);
+    text += 20;
+		
+    while (*text != '\n') { text++; }
+    text++;
+
+	M = NewMatrix(n, m, 0);
+
+	int amt = ReadVectorStr(text, n+1, M.col, colnum, colsize);
+    text += amt;
+
+	amt = ReadVectorStr(text, m, M.row, rownum, rowsize);
+    text += amt;
+
+	for (i=0; i<n; i++)
+		ISort(M, i);
+
+	for (i=0; i<=n; i++)
+	  M.startrow[i] = M.col[i];
+
+	F = LowerToFull(M);
+
+	maxm = 0;
+	for (i=0; i<n; i++)
+	  if (F.col[i+1]-F.col[i] > maxm)
+	    maxm = F.col[i+1]-F.col[i];
+
+	if (F.nz) {
+	  for (j=0; j<n; j++)
+	    for (i=F.col[j]; i<F.col[j+1]; i++)
+	      F.nz[i] = Value(F.row[i], j);
+	}
+
+	FreeMatrix(M);
+
+	return(F);
+}
+
 void DumpLine(FILE *fp)
 {
 	long c;
@@ -219,6 +359,32 @@ void ParseIntFormat(char *buf, long *num, long *size)
   while (*tmp++ != 'I')
     ;
   sscanf(tmp, "%ld", size);
+}
+
+int ReadVectorStr(const char *text, long n, long *where, long perline, long persize)
+{
+  long i, j, k, item;
+  char tmp, buf[100];
+  const char *start = text;
+
+  i = 0;
+  while (i < n) {
+    //fgets(buf, 100, fp);    /* read a line at a time */
+    for (k = 0; k < 99 && *text != '\n'; k++) {
+        buf[k] = *text;
+        text++;
+    }
+    text++;
+    buf[k] = 0;
+    for (j=0; j<perline && i<n; j++) {
+      tmp = buf[(j+1)*persize]; buf[(j+1)*persize] = 0;  /* null terminate */
+      item = atoi(&buf[j*persize]);
+      buf[(j+1)*persize] = tmp;
+      where[i++] = item-1;
+    }
+  }
+
+  return text - start;
 }
 
 
